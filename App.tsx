a6485dd2d86ffbfc2efbce8ade329eeeeec61d2f
Belen JN <belenjarabanavas31@gmail.com>
2022-12-15 16:22:06 +0100

    Stack installation done

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Navigator} from './src/navigator/Navigator';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   );
 };
 
