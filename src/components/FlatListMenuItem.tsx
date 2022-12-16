9ed678ec4323e762b5d096772932b9f91b5c3872
Belen JN <belenjarabanavas31@gmail.com>
2022-12-16 16:30:18 +0100

    New screens created

--- src/components/FlatListMenuItem.tsx
+++ src/components/FlatListMenuItem.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {MenuItem} from '../interfaces/interfaces';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 interface Props {
   menuItem: MenuItem;
@@ -8,10 +11,29 @@ interface Props {
 
 export const FlatListMenuItem = ({menuItem}: Props) => {
   return (
-    <View>
-      <Text>
-        {menuItem.name} - {menuItem.icon}
-      </Text>
-    </View>
+    <TouchableOpacity activeOpacity={0.8}>
+      <View style={styles.container}>
+        <Icon name={menuItem.icon} color="black" size={23} />
+        <Text style={styles.itemText}>{menuItem.name}</Text>
+        <View style={{flex: 1}} />
+        <Icon
+          name="chevron-forward-outline"
+          color="black"
+          size={23}
+          style={{alignSelf: 'flex-end'}}
+        />
+      </View>
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  itemText: {
+    marginLeft: 10,
+    alignSelf: 'center',
+    fontSize: 19,
+  },
+});
