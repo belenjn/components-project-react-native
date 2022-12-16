eaeca7fa0625844ef9a39405e26d3517ef59775f
Belen JN <belenjarabanavas31@gmail.com>
2022-12-16 15:33:40 +0100

    FlatList rendered

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,40 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import {Text, View} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
+// import Icon from 'react-native-vector-icons/Ionicons';
+
+interface MenuItem {
+  name: string;
+  icon: string;
+  compontent?: string;
+}
+
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen ',
+  },
+];
 
 export const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>
+          {menuItem.name} - {menuItem.icon}
+        </Text>
+      </View>
+    );
+  };
   return (
-    <View>
-      <Icon name="star-outline" size={30} color="black" />
+    <View style={{flex: 1}}>
+      <FlatList
+        data={menuItems}
+        renderItem={({item}) => renderMenuItem(item)}
+        keyExtractor={item => item.name}
+      />
     </View>
   );
 };
