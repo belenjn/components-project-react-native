a97ffd031cf81577fe6cee5a6e38dc6dc4e8627f
Belen JN <belenjarabanavas31@gmail.com>
2022-12-18 16:00:12 +0100

    Form created

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
 import {HeaderTitle} from '../components/HeaderTitle';
+import {styles} from '../theme/appTheme';
 
 export const TextInputScreen = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+
   return (
-    <View>
+    <View style={styles.globalMargin}>
       <HeaderTitle title="TextInputs" />
+      <TextInput style={stylesScreen.inputStyle} />
+      <TextInput style={stylesScreen.inputStyle} />
+      <TextInput style={stylesScreen.inputStyle} />
+      <HeaderTitle title={JSON.stringify(form, null, 3)} />
     </View>
   );
 };
+
+const stylesScreen = StyleSheet.create({
+  inputStyle: {
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'rgba(0, 0, 0, 0.3)',
+    height: 50,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+  },
+});
