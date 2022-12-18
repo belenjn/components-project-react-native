7359ce6a49438571d74b0ff9ce8d8e3e3d3f3950
Belen JN <belenjarabanavas31@gmail.com>
2022-12-18 02:23:42 +0100

    HeaderTitle created and used

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,24 +1,14 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {FlatListMenuItem} from '../components/FlatListMenuItem';
+import {HeaderTitle} from '../components/HeaderTitle';
 import {menuItems} from '../data/menuItems';
 // import Icon from 'react-native-vector-icons/Ionicons';
 import {styles} from '../theme/appTheme';
 
 export const HomeScreen = () => {
-  const {top} = useSafeAreaInsets();
-
-  const renderListHeader = () => {
-    return (
-      <View style={{marginTop: top + 20, marginBottom: 20}}>
-        <Text style={styles.title}>Menu's options</Text>
-      </View>
-    );
-  };
-
   const itemSeparator = () => {
     return (
       <View
@@ -37,7 +27,7 @@ export const HomeScreen = () => {
         data={menuItems}
         renderItem={({item}) => <FlatListMenuItem menuItem={item} />}
         keyExtractor={item => item.name}
-        ListHeaderComponent={() => renderListHeader()}
+        ListHeaderComponent={() => <HeaderTitle title="Menu's Options" />}
         ItemSeparatorComponent={() => itemSeparator()}
       />
     </View>
