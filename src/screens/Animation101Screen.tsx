4fa2a0b7833a8720f12e1e341381f18d89139a25
Belen JN <belenjarabanavas31@gmail.com>
2022-12-18 01:42:37 +0100

    useAnimation updated

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -2,34 +2,11 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useRef} from 'react';
 import {Animated, Button, Easing, StyleSheet, View} from 'react-native';
+import {useAnimation} from '../hooks/useAnimation';
 
 export const Animation101Screen = () => {
-  const opacity = useRef(new Animated.Value(0)).current;
-  const top = useRef(new Animated.Value(-100)).current;
-
-  const fadeIn = () => {
-    Animated.timing(opacity, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-
-    Animated.timing(top, {
-      toValue: 0,
-      duration: 700,
-      useNativeDriver: true,
-      easing: Easing.bounce,
-    }).start();
-  };
-
-  const fadeOut = () => {
-    Animated.timing(opacity, {
-      toValue: 0,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-  };
-
+  const {opacity, position, fadeIn, fadeOut, startMovingPosition} =
+    useAnimation();
   return (
     <View style={styles.container}>
       <Animated.View
@@ -37,10 +14,16 @@ export const Animation101Screen = () => {
           ...styles.purpleBox,
           opacity,
           marginBottom: 20,
-          transform: [{translateY: top}],
+          transform: [{translateY: position}],
         }}
       />
-      <Button title="Fade In" onPress={fadeIn} />
+      <Button
+        title="Fade In"
+        onPress={() => {
+          fadeIn();
+          startMovingPosition(100);
+        }}
+      />
       <Button title="Fade Out" onPress={fadeOut} />
     </View>
   );
