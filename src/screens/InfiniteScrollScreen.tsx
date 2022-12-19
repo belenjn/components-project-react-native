b0735418ef6bfced46bc436483cffa691a5d8251
Belen JN <belenjarabanavas31@gmail.com>
2022-12-19 15:52:44 +0100

    InfiniteScroll with numbers

--- src/screens/InfiniteScrollScreen.tsx
+++ src/screens/InfiniteScrollScreen.tsx
@@ -1,12 +1,42 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import {View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
+import {useState} from 'react';
 
 export const InfiniteScrollScreen = () => {
+  const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+
+  const loadMore = () => {
+    const newArray: number[] = [];
+
+    for (let i = 0; i < 5; i++) {
+      newArray[i] = numbers.length + i;
+    }
+    setNumbers([...numbers, ...newArray]);
+  };
+
+  const renderItem = (item: number) => {
+    return <Text style={stylesScroll.textItem}>{item}</Text>;
+  };
   return (
-    <View style={styles.globalMargin}>
-      <HeaderTitle title="Infinite Scroll" />
+    <View style={{...styles.globalMargin, flex: 1}}>
+      <FlatList
+        data={numbers}
+        keyExtractor={item => item.toString()}
+        renderItem={({item}) => renderItem(item)}
+        ListHeaderComponent={<HeaderTitle title="Infinite Scroll" />}
+        onTouchEnd={loadMore}
+        onEndReachedThreshold={0.5} // el 0.5 es la mitad de la screen
+      />
     </View>
   );
 };
+
+const stylesScroll = StyleSheet.create({
+  textItem: {
+    height: 150,
+  },
+});
