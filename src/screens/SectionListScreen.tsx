33456b4961cb8ac68bbb92be4b76fad17970be31
Belen JN <belenjarabanavas31@gmail.com>
2022-12-19 12:18:00 +0100

    New properties in SectionList in SectionListScreen

--- src/screens/SectionListScreen.tsx
+++ src/screens/SectionListScreen.tsx
@@ -1,12 +1,87 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View} from 'react-native';
-import {HeaderTitle} from '../components/HeaderTitle';
+import {SectionList, Text, View} from 'react-native';
+// import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
+import {HeaderTitle} from '../components/HeaderTitle';
+
+interface Casas {
+  casa: string;
+  data: string[];
+}
+
+const casas: Casas[] = [
+  {
+    casa: 'DC Comics',
+    data: [
+      'Batman',
+      'Superman',
+      'Robin',
+      'Batman',
+      'Superman',
+      'Robin',
+      'Batman',
+      'Superman',
+      'Robin',
+      'Batman',
+      'Superman',
+      'Robin',
+    ],
+  },
+  {
+    casa: 'Marvel Comics',
+    data: [
+      'Antman',
+      'Thor',
+      'Spiderman',
+      'Antman',
+      'Antman',
+      'Thor',
+      'Spiderman',
+      'Antman',
+      'Antman',
+      'Thor',
+      'Spiderman',
+      'Antman',
+      'Antman',
+      'Thor',
+      'Spiderman',
+      'Antman',
+    ],
+  },
+  {
+    casa: 'Anime',
+    data: [
+      'Kenshin',
+      'Goku',
+      'Saitama',
+      'Kenshin',
+      'Goku',
+      'Saitama',
+      'Kenshin',
+      'Goku',
+      'Saitama',
+      'Kenshin',
+      'Goku',
+      'Saitama',
+    ],
+  },
+];
 
 export const SectionListScreen = () => {
   return (
-    <View style={styles.globalMargin}>
-      <HeaderTitle title="Section List" />
+    <View style={{...styles.globalMargin, flex: 1}}>
+      <SectionList
+        sections={casas}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({item}) => <Text>{item}</Text>}
+        stickySectionHeadersEnabled
+        renderSectionHeader={({section}) => (
+          <View style={{backgroundColor: 'white'}}>
+            <HeaderTitle title={section.casa} />
+          </View>
+        )}
+      />
     </View>
   );
 };
