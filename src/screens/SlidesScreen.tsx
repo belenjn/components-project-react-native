bfd73a832c5de3de2059a35ca338fe268cc9d674
Belen JN <belenjarabanavas31@gmail.com>
2022-12-19 17:10:58 +0100

    New dependences to solve Carousel's error

--- src/screens/SlidesScreen.tsx
+++ src/screens/SlidesScreen.tsx
@@ -1,12 +1,74 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View} from 'react-native';
-import {HeaderTitle} from '../components/HeaderTitle';
-import {styles} from '../theme/appTheme';
+import Carousel from 'react-native-snap-carousel';
+import {
+  Dimensions,
+  Image,
+  ImageSourcePropType,
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
+interface Slide {
+  title: string;
+  desc: string;
+  img: ImageSourcePropType;
+}
+
+export const items: Slide[] = [
+  {
+    title: 'Titulo 1',
+    desc: 'Ea et eu enim fugiat sunt reprehenderit sunt aute quis tempor ipsum cupidatat et.',
+    img: require('../assets/slide-1.png'),
+  },
+  {
+    title: 'Titulo 2',
+    desc: 'Anim est quis elit proident magna quis cupidatat culpa labore Lorem ea. Exercitation mollit velit in aliquip tempor occaecat dolor minim amet dolor enim cillum excepteur. ',
+    img: require('../assets/slide-2.png'),
+  },
+  {
+    title: 'Titulo 3',
+    desc: 'Ex amet duis amet nulla. Aliquip ea Lorem ea culpa consequat proident. Nulla tempor esse ad tempor sit amet Lorem. Velit ea labore aute pariatur commodo duis veniam enim.',
+    img: require('../assets/slide-3.png'),
+  },
+];
 
 export const SlidesScreen = () => {
+  const renderItem = (item: Slide) => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'white',
+          borderRadius: 5,
+          padding: 40,
+          justifyContent: 'center',
+        }}>
+        <Image
+          source={item.img}
+          style={{
+            width: 350,
+            height: 400,
+            resizeMode: 'center',
+          }}
+        />
+      </View>
+    );
+  };
   return (
-    <View style={styles.globalMargin}>
-      <HeaderTitle title="Slides" />
-    </View>
+    <SafeAreaView style={{flex: 1, paddingTop: 50}}>
+      <Carousel
+        data={items}
+        renderItem={({item}: any) => renderItem(item)}
+        sliderWidth={width}
+        itemWidth={width}
+        vertical={false}
+        layout="default"
+      />
+    </SafeAreaView>
   );
 };
